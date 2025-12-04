e9a366a6175b3a986b3145fbd7b9c85bc56ba8c1
AK Maurya <143257696+ankit-kr-maurya-82@users.noreply.github.com>
2025-12-04 13:58:20 +0530

    first commit

---
+++ FoodMenu/Cafe.java
@@ -0,0 +1,12 @@
+public class Cafe{
+    public static void displayTimeMenu(){
+        System.out.println("------------------Time-----------------");
+        System.out.println("Say Good Morning [1]");
+        System.out.println("Say Good Afternoon [2]");
+        System.out.println("Say Good Evening [3]");
+    }
+    
+    public static void main(String[] args) {
+         displayTimeMenu();
+    }
+}
