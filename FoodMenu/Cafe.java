c2aa02d68ce65ab2c782fe98e86d113c78bf9377
AK Maurya <143257696+ankit-kr-maurya-82@users.noreply.github.com>
2025-12-19 20:25:44 +0530

    first nested function for condition for food Time

--- FoodMenu/Cafe.java
+++ FoodMenu/Cafe.java
@@ -15,19 +15,46 @@ public class Cafe{
         switch(time) {
             case 1:
                 System.out.println("\nGood Morning");
+                class foodItemContainer {
+                    public void foodItem(){
+                        System.out.println("Drinks [1]");
+                        System.out.println("Snacks [2]");
+                    }
+                }
+                foodItemContainer foodObject = new foodItemContainer();
+                foodObject.foodItem();
                 break;
             case 2:
                 System.out.println("\nGood Afternoon");
+                class foodItemContainerTwo {
+                    public void foodItemTwo(){
+                        System.out.println("Chinise Dish [1]");
+                        System.out.println("South Dish [2]");
+                    }
+                }
+                foodItemContainerTwo foodObjectTwo = new foodItemContainerTwo();
+                foodObjectTwo.foodItemTwo();
                 break;
             case 3:
                 System.out.println("\nGood Evening");
+                class foodItemContainerThree {
+                    public void foodItemThree(){
+                        System.out.println("Special Thali [1]");
+                        System.out.println("Normal Thali [2]");
+                    }
+                }
+                foodItemContainerThree foodObjectThree = new foodItemContainerThree();
+                foodObjectThree.foodItemThree();
                 break;
             default:
                 throw new AssertionError();
         }
     }
+
+    // public static void 
     public static void main(String[] args) {
-         displayTimeMenu();
-         getUserTime();
+        displayTimeMenu();
+        getUserTime();
+
     }
 }
