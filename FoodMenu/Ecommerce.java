3137be690ec8727b816f31ebdcf1546ec5394089
AK Maurya <ankitkraurya82@gmail.com>
2026-09-19 19:56:47 +0530

    add Ecommerce project

---
+++ FoodMenu/Ecommerce.java
@@ -0,0 +1,20 @@
+class Ecommerce {
+
+    public static void line(int l){
+        for(int i=0;i<l;i++){
+            System.out.print("-");
+        }
+    }
+
+    public static void linebreak(){
+        System.out.println();
+    }
+    public static void main(String[] args){
+        line(30);
+        linebreak();
+        System.out.println("Ecommerce Platform");
+        line(30);
+        linebreak();
+        
+    }
+}
